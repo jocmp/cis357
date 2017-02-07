89d22356c3fd978b488d63d5c92644b5dd252c76
Josiah Campbell <josiahcampbell64@gmail.com>
2017-02-06 19:56:35 -0500

    Update color scheme, add clear-all button

--- homework/homework3/app/src/main/java/edu/gvsu/cis/campbjos/homework3/MainActivity.java
+++ homework/homework3/app/src/main/java/edu/gvsu/cis/campbjos/homework3/MainActivity.java
@@ -2,31 +2,22 @@ package edu.gvsu.cis.campbjos.homework3;
 
 import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.util.Locale;
 
 import edu.gvsu.cis.campbjos.homework3.widget.CoordinateLinearLayout;
 
-import static java.lang.String.format;
 import static java.math.RoundingMode.HALF_EVEN;
-import static java.math.RoundingMode.HALF_UP;
 
+/**
+ * @author Josiah Campbell
+ * @author Derrick Bentley
+ */
 public class MainActivity extends AppCompatActivity {
-
-
-    private static final String BEARING_FORMAT = "%.2f degrees";
-    private static final String DISTANCE_FORMAT = "%.2f kilometers";
 
     private CoordinateLinearLayout sourceCoordinates;
     private CoordinateLinearLayout destinationCoordinates;
@@ -41,18 +32,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Geo Locator");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> calculate());
-
         sourceCoordinates = (CoordinateLinearLayout) findViewById(R.id.sourceCoordinates);
         destinationCoordinates = (CoordinateLinearLayout) findViewById(R.id.destinationCoordinates);
 
+
+        bindResultText();
+        bindButtons();
+    }
+
+    private void bindResultText() {
         bearingText = (TextView) findViewById(R.id.bearingText);
         distanceText = (TextView) findViewById(R.id.distanceText);
+        resetResults();
+    }
 
-        sourceCoordinates.prepopulate("43.077366", "-85.994053");
-        destinationCoordinates.prepopulate("43.077303", "-85.993860");
-        calculate();
+    private void bindButtons() {
+        Button clearAll = (Button) findViewById(R.id.clearAll);
+        clearAll.setOnClickListener(view -> clearAll());
+
+        Button calculate = (Button) findViewById(R.id.calculate);
+        calculate.setOnClickListener(view -> calculate());
     }
 
     private void calculate() {
@@ -62,11 +61,23 @@ public class MainActivity extends AppCompatActivity {
         float distance = round(source.distanceTo(destination)) / 1000.0f;
         float bearing = source.bearingTo(destination);
 
-        bearingText.setText(format(Locale.getDefault(), BEARING_FORMAT, bearing));
-        distanceText.setText(format(Locale.getDefault(), DISTANCE_FORMAT, distance));
+        bearingText.setText(getString(R.string.degrees, bearing));
+        distanceText.setText(getString(R.string.kilometers, distance));
     }
 
-    private float round(double value) {
+private float round(floatjjqq value) {
         return BigDecimal.valueOf(value).setScale(2, HALF_EVEN).floatValue();
+    }
+
+    private void clearAll() {
+        sourceCoordinates.clearFields();
+        destinationCoordinates.clearFields();
+
+        resetResults();
+    }
+
+    private void resetResults() {
+        bearingText.setText(getString(R.string.degrees, 0f));
+        distanceText.setText(getString(R.string.kilometers, 0f));
     }
 }
